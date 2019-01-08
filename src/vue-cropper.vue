a523a2573312e964a5ac7e7066576f72101dc00b
coderkai <397340297@qq.com>
2019-01-08 12:47:16 +0800

    Update vue-cropper.vue

--- src/vue-cropper.vue
+++ src/vue-cropper.vue
@@ -14,6 +14,7 @@
 					:src="imgs"
 					alt="cropper-img"
 					ref="cropperImg"
+					crossorigin="anonymous"
 					/>
 			</div>
 		</div>
@@ -42,6 +43,7 @@
 						}"
 						:src="imgs"
 						alt="cropper-img"
+						crossorigin="anonymous"
 						/>
 				</span>
 				<span
